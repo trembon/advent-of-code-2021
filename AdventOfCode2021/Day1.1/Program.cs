b0222af6c0e88d4f88b1b1c91045197aa0ff362d
trembon <trembon@hotmail.com>
2021-12-01 22:58:12 +0100

    day 1-1

---
+++ AdventOfCode2021/Day1.1/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int[] measurements = lines.Select(x => int.Parse(x)).ToArray();
+
+int measurementIncreases = 0;
+for(int i = 1; i < measurements.Length; i++)
+    if(measurements[i] > measurements[i - 1])
+        measurementIncreases++;
+
+Console.WriteLine($"measurementIncreases: {measurementIncreases}");
