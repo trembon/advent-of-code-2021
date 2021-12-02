8ef42a1cf5c0f2c651c5ff6d2cae9515c65b3052
trembon <trembon@hotmail.com>
2021-12-02 09:25:26 +0100

    day 1-2

---
+++ AdventOfCode2021/Day1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int[] measurements = lines.Select(x => int.Parse(x)).ToArray();
+
+const int slidingSize = 3;
+
+List<int> slidingMeasurements = new();
+for(int i = slidingSize - 1; i < measurements.Length; i++)
+{
+    Range range = new(i - (slidingSize - 1), i + 1);
+    slidingMeasurements.Add(measurements[range].Sum());
+}
+
+int slidingMeasurementIncreases = 0;
+for (int i = 1; i < slidingMeasurements.Count; i++)
+    if (slidingMeasurements[i] > slidingMeasurements[i - 1])
+        slidingMeasurementIncreases++;
+
+Console.WriteLine($"measurementIncreases: {slidingMeasurementIncreases}");
