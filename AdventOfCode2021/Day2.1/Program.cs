26d42ebf7d4501f9131763d0eead57172dfbb9f4
trembon <trembon@hotmail.com>
2021-12-02 09:33:47 +0100

    day 2-1

---
+++ AdventOfCode2021/Day2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] instructions = File.ReadAllLines("input.txt");
+
+int depthPosition = 0;
+int horizontalPosition = 0;
+
+foreach(var instruction in instructions)
+{
+    string[] vs = instruction.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    int value = int.Parse(vs[1]);
+
+    switch (vs[0])
+    {
+        case "up":
+            depthPosition -= value;
+            break;
+
+        case "down":
+            depthPosition += value;
+            break;
+
+        case "forward":
+            horizontalPosition += value;
+            break;
+
+        default:
+            Console.WriteLine("Invalid command: " + instruction);
+            break;
+    }
+}
+
+Console.WriteLine("---Result---");
+Console.WriteLine($"Depth: {depthPosition}");
+Console.WriteLine($"Horizontal: {horizontalPosition}");
+Console.WriteLine($"Calculation: {depthPosition * horizontalPosition}");
