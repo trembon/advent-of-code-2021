b3fdc9558cf55affc28d9da38789396404dedb66
trembon <trembon@hotmail.com>
2021-12-02 09:38:45 +0100

    day 2-2

---
+++ AdventOfCode2021/Day2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] instructions = File.ReadAllLines("input.txt");
+
+int depthPosition = 0;
+int horizontalPosition = 0;
+int aim = 0;
+
+foreach (var instruction in instructions)
+{
+    string[] vs = instruction.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    int value = int.Parse(vs[1]);
+
+    switch (vs[0])
+    {
+        case "up":
+            aim -= value;
+            break;
+
+        case "down":
+            aim += value;
+            break;
+
+        case "forward":
+            horizontalPosition += value;
+            depthPosition += aim * value;
+            break;
+    }
+}
+
+Console.WriteLine("---Result---");
+Console.WriteLine($"Depth: {depthPosition}");
+Console.WriteLine($"Horizontal: {horizontalPosition}");
+Console.WriteLine($"Calculation: {depthPosition * horizontalPosition}");
