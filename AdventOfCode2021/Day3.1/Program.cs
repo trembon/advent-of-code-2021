84adc96068aa48d545cae563442f839d43acc3d8
trembon <trembon@hotmail.com>
2021-12-03 08:34:45 +0100

    day 3-1

---
+++ AdventOfCode2021/Day3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] diagnostics = File.ReadAllLines("input.txt");
+
+
+List<char> gamma = new();
+List<char> epsilon = new();
+for(int i = 0; i < diagnostics[0].Length; i++)
+{
+    var common = diagnostics.Select(x => x[i]).GroupBy(x => x).Select(x => new { x.Key, Count = x.Count() }).OrderByDescending(x => x.Count).ToList();
+    gamma.Add(common[0].Key);
+    epsilon.Add(common[1].Key);
+}
+
+int gammaValue = Convert.ToInt32(new string(gamma.ToArray()), 2);
+int epsilonValue = Convert.ToInt32(new string(epsilon.ToArray()), 2);
+
+Console.WriteLine($"consumption: {gammaValue * epsilonValue}");
