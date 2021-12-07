1145485ed69839d3a512cf642e44c84f55f3004f
trembon <trembon@hotmail.com>
2021-12-07 10:48:07 +0100

    day 7-2

---
+++ AdventOfCode2021/Day7.2/Program.cs
@@ -0,0 +1,16 @@
+﻿string data = File.ReadAllText("input.txt");
+
+var crabPositions = data.Split(',').Select(x => int.Parse(x)).ToList();
+
+List<int> fuelUsageCalculations = new List<int>();
+for(int avg = crabPositions.Min(); avg <= crabPositions.Max(); avg++)
+{
+    int fuelUsage = 0;
+    for(int i = 0; i < crabPositions.Count; i++)
+        fuelUsage += Enumerable.Range(0, Math.Abs(crabPositions[i] - avg) + 1).Sum();
+
+    fuelUsageCalculations.Add(fuelUsage);
+}
+
+
+Console.WriteLine($"fuel usage: {fuelUsageCalculations.Min()}");
