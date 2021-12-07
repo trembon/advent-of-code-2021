063749dbf62ad87ffbd8355838b0c6b961541f22
trembon <trembon@hotmail.com>
2021-12-07 10:24:15 +0100

    day 7-1

---
+++ AdventOfCode2021/Day7.1/Program.cs
@@ -0,0 +1,13 @@
+﻿string data = File.ReadAllText("input.txt");
+
+var crabPositions = data.Split(',').Select(x => int.Parse(x)).ToList();
+
+int median = crabPositions.OrderBy(x => x).ElementAt(crabPositions.Count / 2);
+int fuelUsage = 0;
+foreach(var position in crabPositions)
+{
+    int calc = median - position;
+    fuelUsage += Math.Abs(calc);
+}
+
+Console.WriteLine($"fuel usage: {fuelUsage}");
