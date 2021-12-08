84535fb71f1210abaaf00bc22bd85daf417cbcef
trembon <trembon@hotmail.com>
2021-12-08 09:14:12 +0100

    day 8-1

---
+++ AdventOfCode2021/Day8.1/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] data = File.ReadAllLines("input.txt");
+
+List<string> outputValues = new(data.Length);
+foreach(var line in data)
+{
+    string[] split = line.Split(" | ");
+    outputValues.AddRange(split[1].Split(" "));
+}
+
+int count = outputValues.Count(x => x.Length == 2 || x.Length == 3 || x.Length == 4 || x.Length == 7);
+Console.WriteLine($"counted output values: {count}");
