cf5263741221c567c3dfc02f0c0fbab4f1f0b661
trembon <trembon@hotmail.com>
2021-12-11 10:48:42 +0100

    day 11-2

---
+++ AdventOfCode2021/Day11.2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+string[] data = File.ReadAllLines("input.txt");
+int[,] grid = new int[data.Length, data[0].Length];
+
+for (int y = 0; y < data.Length; y++)
+    for (int x = 0; x < data[y].Length; x++)
+        grid[y, x] = (int)char.GetNumericValue(data[y][x]);
+
+int firstSyncFlash = 0;
+for (int i = 0; firstSyncFlash == 0; i++)
+{
+    HashSet<(int y, int x)> flashed = new();
+    for (int y = 0; y < grid.GetLength(0); y++)
+        for (int x = 0; x < grid.GetLength(1); x++)
+            IncreaseAndFlash(y, x, flashed);
+
+    foreach (var flash in flashed)
+        grid[flash.y, flash.x] = 0;
+
+    if(flashed.Count == grid.GetLength(0) * grid.GetLength(1))
+    {
+        firstSyncFlash = i + 1;
+        break;
+    }
+}
+
+Console.WriteLine("first sync flash " + firstSyncFlash);
+
+void IncreaseAndFlash(int y, int x, HashSet<(int y, int x)> flashed)
+{
+    grid[y, x]++;
+    if (grid[y, x] > 9 && !flashed.Contains(new(y, x)))
+    {
+        flashed.Add(new(y, x));
+        ProcessAdjecents(y, x, flashed);
+    }
+}
+
+void ProcessAdjecents(int y, int x, HashSet<(int y, int x)> flashed)
+{
+    // left
+    if (x > 0)
+        IncreaseAndFlash(y, x - 1, flashed);
+
+    // right
+    if (x < data[y].Length - 1)
+        IncreaseAndFlash(y, x + 1, flashed);
+
+    // up
+    if (y > 0)
+        IncreaseAndFlash(y - 1, x, flashed);
+
+    // down
+    if (y < data.Length - 1)
+        IncreaseAndFlash(y + 1, x, flashed);
+
+    // left - up
+    if (x > 0 && y > 0)
+        IncreaseAndFlash(y - 1, x - 1, flashed);
+
+    // left - down
+    if (x > 0 && y < data.Length - 1)
+        IncreaseAndFlash(y + 1, x - 1, flashed);
+
+    // right - up
+    if (x < data[y].Length - 1 && y > 0)
+        IncreaseAndFlash(y - 1, x + 1, flashed);
+
+    // right - down
+    if (x < data[y].Length - 1 && y < data.Length - 1)
+        IncreaseAndFlash(y + 1, x + 1, flashed);
+}
