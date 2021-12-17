ee57f5b1ec8a2ea7c466a4f0463d0955f4420f0a
trembon <trembon@hotmail.com>
2021-12-17 20:17:18 +0100

    day 17-1

---
+++ AdventOfCode2021/Day17.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+string data = File.ReadAllText("input.txt");
+
+var matchX = Regex.Match(data, "x=(-?\\d*)\\.\\.(\\d*),");
+var matchY = Regex.Match(data, "y=(-?\\d*)\\.\\.(-?\\d*)");
+
+int[] rangeX = new int[] { int.Parse(matchX.Groups[1].Value), int.Parse(matchX.Groups[2].Value) }.OrderBy(x => x).ToArray();
+int[] rangeY = new int[] { int.Parse(matchY.Groups[1].Value), int.Parse(matchY.Groups[2].Value) }.OrderBy(x => x).ToArray();
+
+int highestYPosition = 0;
+
+int xLower = 1;
+while ((xLower * (xLower + 1)) / 2 < rangeX.First()) xLower++;
+
+for (int x = xLower; x <= rangeX.Last(); x++)
+{
+    for (int y = rangeY.First(); y < Math.Abs(rangeY.First()); y++)
+    {
+        int xVel = x, yVel = y;
+        int xPos = 0, yPos = 0;
+        int highestYThisCombo = 0;
+
+        while (yPos >= rangeY.First() && xPos <= rangeX.Last())
+        {
+            xPos += xVel; yPos += yVel;
+            highestYThisCombo = Math.Max(highestYThisCombo, yPos); 
+            if (xPos >= rangeX.First() && xPos <= rangeX.Last() && yPos <= rangeY.Last() && yPos >= rangeY.First())
+            {
+                highestYPosition = Math.Max(highestYPosition, highestYThisCombo);
+            }
+            xVel = (xVel == 0) ? 0 : xVel - 1;
+            yVel--;
+        }
+    }
+}
+
+Console.WriteLine($"highest y: {highestYPosition}");
